659a76c2f99378c832141e2ba9d690f08342656d
RedneckDidit <nabilkandani@gmail.com>
2026-02-24 12:53:51 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    bigip = {
+      source = "terraform-providers/bigip"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "bigip" {
+  address  = var.hostname
+  username = var.username
+  password = var.password
+}
