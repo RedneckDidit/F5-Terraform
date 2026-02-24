22926541ef89fabcda6848a68b743ba94a1ff652
RedneckDidit <nabilkandani@gmail.com>
2026-02-24 13:04:12 +0000

    Update pool.tf

--- pool.tf
+++ pool.tf
@@ -9,3 +9,8 @@ resource "bigip_ltm_pool" "pool" {
   minimum_active_members = 1
   monitors               = [bigip_ltm_monitor.monitor.name]
 }
+
+resource "bigip_ltm_pool_attachment" "ipv4_node_attach" {
+  pool = bigip_ltm_pool.pool.name
+  node = "1.1.1.1:80"
+}
